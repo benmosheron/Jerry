8a2f12c1f81fae94f6b10cfe333bc6ab83df2c30
Ben Sheron <benmosheron@users.noreply.github.com>
2018-05-12 12:00:22 +0100

    Draw one petal per frame

--- life/flower.js
+++ life/flower.js
@@ -3,24 +3,44 @@
 // Lets provide a generator and engine
 function Flower(config, canvasController){
     const centre = new Vector([config.canvasWidth / 2 , config.canvasHeight / 2]).transpose()
+
     this.generate = function(){
         // Our state will be an array of petals, and our run function
         // We probably don't need to store an array or positions, as we can calculate them each time from the parameters,
         const petals = placePetals(1000,2*Math.PI/2.12318723821127,1.0)
-        render(petals)
+        // Draw the centre
+        canvasController.drawSquare(centre.collapse().x, centre.collapse().y, 10, "rgb(180,30,40)")
+        renderAll(petals)
+        let point_mut = 0
         return {
             run: function(){
-                // render(petals)
+                // We can reposition the petals based on input parameters
+                if(point_mut>=petals.length) point_mut = 0
+                render(petals, point_mut)
+                point_mut++
             },
             petals: petals 
         }
     }
 
-    function render(petals){
-        canvasController.drawSquare(centre.collapse().x, centre.collapse().y, 10, "rgb(180,30,40)")
+    // HOT PATH
+    function render(petals, i){
+        const p = petals[i]
+        // Draw one petal per iteration (avois horrible performance, see commented code below)...
+        canvasController.drawSquareRandom(p.array[0].array[0], p.array[1].array[0], 10)
+        // petals
+        //     // .map(p => p.collapse()) // collapse column vectors ([[x],[y]]) to flat vector ([x,y]) // OH GOD THE PERFORMANCE!! *cries*
+        //     // .map((p, i) => canvasController.drawSquareRandom(p.x, p.y, 10))
+        //     // hack the values out of the column vector to avoid the horrible ben-loves-vectors performance
+        //     .map(p => canvasController.drawSquareRandom(p.array[0].array[0], p.array[1].array[0], 10))
+    }
+
+    function renderAll(petals){
         petals
-            .map(p => p.collapse()) // collapse column vectors ([[x],[y]]) to flat vector ([x,y])
-            .map((p, i) => canvasController.drawSquareRandom(p.x, p.y, 10, debugColour(i)))
+        //     // .map(p => p.collapse()) // collapse column vectors ([[x],[y]]) to flat vector ([x,y]) // OH GOD THE PERFORMANCE!! *cries*
+        //     // .map((p, i) => canvasController.drawSquareRandom(p.x, p.y, 10))
+            // hack the values out of the column vector to avoid the horrible ben-loves-vectors performance
+            .map(p => canvasController.drawSquareRandom(p.array[0].array[0], p.array[1].array[0], 10))
     }
 
     // n = number of petals
